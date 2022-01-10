f5a88d8d1a0d4779b15af7ada701cb575e84528c
cj <shahid.alam57@gmail.com>
2022-01-10 10:50:19 +0530

    added javascript for adding and clearing all tasks

---
+++ app.js
@@ -0,0 +1,54 @@
+let addBtn = document.querySelector('.inputDiv button'),
+inputField = document.querySelector('.inputDiv > input'),
+tasksList = document.querySelector('.task-list'),
+clearBtn = document.querySelector('.lower-part button')
+
+window.onload = function start(){
+    let UncompletedTasks = JSON.parse(localStorage.getItem("Tasks"))
+    if(UncompletedTasks!==null){
+        UncompletedTasks.forEach(task => {
+            let tags = `<li>${task}<div class="icon-wrapper"><i class="fa-solid fa-check"></i></div></li>`
+            tasksList.innerHTML += tags
+        });
+    }
+   
+
+}
+console.log(tasksList);
+
+function checkIfType(){
+    //after removing whitespaces, if there are no characters
+    if(inputField.value.trim()==0){
+        addBtn.classList.remove('active')
+    }
+    else{
+        addBtn.classList.add('active')
+    }
+}
+
+function addNewItem(){
+let arrayOfTasks;
+    let getUncompletedTasks = localStorage.getItem("Tasks")
+    //if the key returns null instead of a value
+    if(getUncompletedTasks==null){
+        arrayOfTasks = []
+    }
+    else{
+        //get the value from its key by first parsing
+        arrayOfTasks = JSON.parse(getUncompletedTasks)
+    }
+    arrayOfTasks.push(inputField.value)
+    localStorage.setItem("Tasks",JSON.stringify(arrayOfTasks))
+    let tags = `<li>${inputField.value}<div class="icon-wrapper"><i class="fa-solid fa-check"></i></div></li>`
+    tasksList.innerHTML += tags
+    inputField.value=""
+    
+}
+function clearList(){
+    localStorage.clear()
+    tasksList.innerHTML = ''
+}
+
+addBtn.addEventListener('click', addNewItem)
+inputField.addEventListener('input',checkIfType)
+clearBtn.addEventListener('click', clearList)
