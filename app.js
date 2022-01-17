342d5ee3d082d14edf44b8501fe7db260f3412e6
cj <shahid.alam57@gmail.com>
2022-01-17 10:48:37 +0530

    added js for complete and delete

--- app.js
+++ app.js
@@ -1,53 +1,91 @@
 let addBtn = document.querySelector(".inputDiv button"),
   inputField = document.querySelector(".inputDiv > input"),
   tasksList = document.querySelector(".task-list"),
+  incompleteTasks = document.querySelector(".task-list ul li"),
   clearBtn = document.querySelector(".lower-part button"),
-  completedList = document.querySelector("completed-tasks");
+  completedList = document.querySelector(".completed-tasks ul");
+  // console.log(completedList);
 let arrayOfTasks,
   arrayOfCompleted = [];
 
+
+
 function start() {
   let UncompletedTasks = JSON.parse(localStorage.getItem("Tasks"));
+  let completedTasks = JSON.parse(localStorage.getItem("CTasks"));
   if (UncompletedTasks !== null) {
     UncompletedTasks.forEach((task) => {
       let tags = `<li>${task}<div class="icon-wrapper"><i class="fa-solid fa-check"></i></div></li>`;
       tasksList.innerHTML += tags;
     });
   }
+  if(completedTasks!==null){
+    completedTasks.forEach(task=>{
+      let tags = `<li>${task}<div class="icon-wrapper"><i class="fa-solid fa-trash"></i></div></li>`
+      completedList.innerHTML += tags;
+    })
+  }
 }
 
-let prom = new Promise(function (successFunc, failureFunc) {
-    let getCompletedTasks = localStorage.getItem("CTasks");
-  //if the key returns null instead of a value
-  if (getCompletedTasks == null) {
-    arrayOfTasks = [];
-  } else {
-    //get the value from its key by first parsing
-    arrayOfTasks = JSON.parse(getUncompletedTasks);
+
+
+
+
+
+
+
+function moveToCompleted(){
+  let getLocalStorage = localStorage.getItem("CTasks")
+  let getLocalStorageTasks = localStorage.getItem("Tasks"),
+  taskArray = JSON.parse(getLocalStorageTasks)
+  if(getLocalStorage==null){
+    arrayOfCompleted=[]
   }
-});
+  else{
+    arrayOfCompleted = JSON.parse(getLocalStorage)
+  }
+  // console.log(taskArray);
+  let txt = this.textContent
+  let index=taskArray.indexOf(txt)
+  taskArray.splice(index,1)
+  localStorage.setItem("Tasks",JSON.stringify(taskArray))
+  arrayOfCompleted.push(txt)
+  localStorage.setItem("CTasks",JSON.stringify(arrayOfCompleted))
+  this.remove()
+  completedList.innerHTML += `<li>${txt}<div class="icon-wrapper"><i class="fa-solid fa-trash"></i></div></li>`
+}
 
-// setInterval(() => {
-//     // console.log(tasksList.children);
-//     Array.from(tasksList.children).forEach(task=>{
-//         task.addEventListener('click',()=>{
-//             console.log(task);
-//         })
-//     })
-// }, 1000);
 
-/**
- * get the element
- * create a local space for completed(if there is none)
- * transfer the element from tasks to completed -> by pushing to array of completed
- * remove the element from task
- * add the array to completed local storage
- * remove element from the current parent
- *
- */
-function moveToCompleted() {}
+
+function move(){
+  Array.from(tasksList.children).forEach(task=>{
+    task.addEventListener('click',moveToCompleted)
+  })
+}
+function moveToo(){
+  Array.from(completedList.children).forEach(task=>{
+    task.addEventListener('click',()=>{
+      console.log(task);
+      let txt= task.textContent
+      array = JSON.parse(localStorage.getItem("CTasks"))
+      if(txt!==undefined){
+      let i=array.indexOf(txt)
+      array.splice(i,1)
+      localStorage.setItem("CTasks",JSON.stringify(array))
+      task.remove()
+      }
+    })
+  })
+}
+
+
+
 
 window.onload = start();
+window.onload = setTimeout(() => {
+  move()
+  moveToo()
+}, 1000);
 
 function checkIfType() {
   //after removing whitespaces, if there are no characters
@@ -76,6 +114,13 @@ function addNewItem() {
     tasksList.innerHTML += tags;
     inputField.value = "";
   }
+
+  setTimeout(move(),1000)
+  setTimeout(moveToo(),1000)
+
+
+
+
 }
 function clearList() {
   localStorage.clear();
